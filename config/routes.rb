b82049cb5f5d030ecdf867d57aea6ea0c52630d0
Jack Inscoe <jackinscoe@gmail.com>
2011-06-30 00:52:36 -0400

    added a stubbed user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 SampleApp::Application.routes.draw do
+  resources :users
+
   root :to => "pages#home"
   
+    
   match '/signup', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
