1cf761fab8847a2f584fb273185bdf8eebc929a7
Jack Inscoe <jackinscoe@gmail.com>
2011-07-01 12:09:39 -0400

    fixing bug

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ SampleApp::Application.routes.draw do
   root :to => "pages#home"
 
   match '/signup', :to => 'users#new'
-  match '/login', :to => 'sessions#new'
+  match '/signin', :to => 'sessions#new'
   match '/signout', :to => 'sessions#destroy'
 
   match '/contact', :to => 'pages#contact'
