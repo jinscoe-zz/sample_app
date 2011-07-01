56c37cb55b9e1eddc1ce0ac53ff77bdd004f112f
Jack Inscoe <jackinscoe@gmail.com>
2011-07-01 11:48:56 -0400

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ SampleApp::Application.routes.draw do
   resources :sessions, :only => [:new, :create, :destroy]
 
   root :to => "pages#home"
-  
+
+  match '/signup', :to => 'users#new'
   match '/signin', :to => 'sessions#new'
   match '/signout', :to => 'sessions#destroy'
-  match '/signup', :to => 'users#new'
+
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
