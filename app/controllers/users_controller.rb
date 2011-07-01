47ba9d626e495f8422a82fba37c0025d99b3fb25
Jack Inscoe <jackinscoe@gmail.com>
2011-07-01 15:25:40 -0400

    access control for edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   
   before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
   
   
   def show
@@ -41,6 +42,13 @@ class UsersController < ApplicationController
   
     def authenticate
       deny_access unless signed_in?
+    end
+    
+    def correct_user
+      
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+      
     end
     
     
