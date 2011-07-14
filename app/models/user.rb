150daa29631a8f2501b37c304264b13cadf9726c
Jack Inscoe <jackinscoe@gmail.com>
2011-07-14 13:47:03 -0400

    user relationships(following/followers)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ActiveRecord::Base
   attr_accessor   :password
   
   has_many :microposts, :dependent  => :destroy
+  has_many :relationships, :dependent => :destroy, :foreign_key => "follower_id" 
+  has_many :reverse_relationships, :dependent  => :destroy, :foreign_key  => "followed_id", :class_name  => "Relationship"
+  has_many :following, :through => :relationships,:source  => :followed 
+  has_many :followers, :through  => :reverse_relationships, :source  => :follower
+  
   
   #will paginate variables
   cattr_reader :per_page
@@ -27,6 +32,18 @@ class User < ActiveRecord::Base
   
   def feed
     Micropost.where("user_id = ?", id)
+  end
+  
+  def following?(followed)
+    self.relationships.find_by_followed_id(followed)
+  end
+  
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+  
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
   end
   
   def self.authenticate(email, submitted_password)    
